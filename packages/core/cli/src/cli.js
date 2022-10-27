da10947f0ee1f290d604c1275ebba1984a299483
chareice <chareice@live.com>
2022-10-27 11:31:41 +0800

    feat: node18 support

--- packages/core/cli/src/cli.js
+++ packages/core/cli/src/cli.js
@@ -5,6 +5,8 @@ const cli = new Command();
 
 cli.version(require('../package.json').version);
 
+process.env.NODE_OPTIONS = '--openssl-legacy-provider --no-experimental-fetch';
+
 commands(cli);
 
 module.exports = cli;
